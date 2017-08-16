98722c6a7a02be116948f4a7e94dcbdd5bc65733
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-16 08:51:01 -0700

    add initial components and keg class as well as fix for app.module to have a basic working page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,18 @@
-import
+import { Component } from '@angular/core';
 
 @Component({
-
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <div class="jumbotron">
+      <h1>Keg Tracker</h1>
+      <h3>{{subtitle}}</h3>
+    </div>
+  </div>
+  `
 })
 
-export class AppComponent { }
+export class AppComponent {
+  subtitle: string = 'Log Pint Pours and Track Keg Status';
+
+}
